567674a57a09a5e9f437ea80aa9cc212f5ae4dae
jarvis-su <jarvis_su@126.com>
2016-04-14 21:32:34 +0800

    add update the database info

---
+++ user_mgmt/src/main/database/day_0.sql
@@ -0,0 +1,3 @@
+INSERT user_types VALUES (1,'admin','admin users',now());
+INSERT INTO status values (1,'enable','user mgmt',now());
+INSERT  into role values(1,'super','super admin',1,1,now(),now());
